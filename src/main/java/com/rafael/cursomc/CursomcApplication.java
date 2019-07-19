a3348821b8d425ea44d32cde87a0193cb9b4b9ae
RafaelClaumann <rafael_claumann@hotmail.com>
2019-07-19 03:24:09 -0300

    Criando operação de instanciação ao iniciar o sistema.

--- src/main/java/com/rafael/cursomc/CursomcApplication.java
+++ src/main/java/com/rafael/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.rafael.cursomc;
 
+import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "informática");
+        Categoria cat2 = new Categoria(null, "escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
